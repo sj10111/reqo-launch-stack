0c87374110fcd18527daee8faf76bf960efea7a9
Srujan <62290482+sj10111@users.noreply.github.com>
2022-08-02 16:16:07 +0530

    Improved design

--- pages/components/EmailSend.js
+++ pages/components/EmailSend.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import axios from "axios";
 import validator from "validator";
@@ -7,6 +7,7 @@ function EmailSend() {
   const [email, setEmail] = useState("");
   const [sent, setSent] = useState(false);
   const [errorMessage, setErrorMessage] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   var data = JSON.stringify({
     email: `${email}`,
@@ -19,13 +20,19 @@ function EmailSend() {
     },
     data: data,
   };
-
-  async function handleClick() {
+  const form = useRef(null);
+  async function handleClick(e) {
+    e.preventDefault();
+    console.log(e.target.value);
+    setEmail("");
     if (validator.isEmail(email)) {
       setErrorMessage(false);
       console.log("Valid Email");
+      setLoading(true);
       await axios(config)
         .then(function (response) {
+          setLoading(false);
+
           console.log(response);
         })
         .catch(function (error) {
@@ -37,8 +44,10 @@ function EmailSend() {
         setSent(false);
       }, 3000);
     } else {
+      setLoading(false);
       setSent(true);
       setErrorMessage(true);
+
       console.log("Invalid Email");
     }
   }
@@ -47,50 +56,79 @@ function EmailSend() {
       <div className="flex font-work-sans tracking-tighter text-[18px] ">
         Be the first to know when we go live
       </div>
-      <div className="flex justify-center">
-        <input
-          value={email.value}
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-          className="font-work-sans text-[18px]  w-[344px] outline-none focus:border-[#3263F3] bg-transparent border-b-2 transition-all duration-500"
-          placeholder="Enter your E-mail"
-        ></input>
-        <motion.button
-          onClick={handleClick}
-          className="bg-[#3263F3] w-[221px] active:scale-105 h-[53px] text-white font-basic-sans rounded-[24px] ml-5 hover:bg-[#1041D0] hover:shadow-md transition-all"
-        >
-          Notify Me
-        </motion.button>
+      <div className="flex justify-center ">
+        <form onSubmit={(e) => handleClick(e)} className="flex ">
+          <input
+            type="text"
+            placeholder="Enter your Email"
+            className="font-work-sans text-[18px]  w-[344px] outline-none focus:border-[#3263F3] bg-transparent border-b-2 transition-all duration-500 "
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            ref={form}
+          />
+          {loading ? (
+            <div class="flex mt-2 items-center justify-center w-[221px] h-[53px] ml-5 transition:all ">
+              <div
+                class="spinner-border  text-[#3263F3] animate-spin inline-block w-4 h-4 border-2 rounded-full"
+                role="status"
+              >
+                <span class="visually-hidden"></span>
+              </div>
+            </div>
+          ) : (
+            <input
+              type="submit"
+              value="Notify me"
+              className="bg-[#3263F3] w-[221px] active:scale-105 h-[53px] text-white font-basic-sans rounded-[24px] ml-5 hover:bg-[#1041D0] hover:shadow-md transition-all"
+            />
+          )}
+        </form>
       </div>
       {sent &&
         (errorMessage ? (
-          <motion.div className="font-work-sans flex items-center mt-4 text-[#3263F3]">
-            <svg
-              width="14"
-              height="15"
-              viewBox="0 0 14 15"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M1 1.75L12.5 13.25"
-                stroke="#EC5D6D"
-                strokeWidth="2"
-                strokeLinecap="round"
-              />
-              <path
-                d="M1 13.25L12.5 1.75"
-                stroke="#EC5D6D"
-                strokeWidth="2"
-                strokeLinecap="round"
-              />
-            </svg>
+          loading ? (
+            <div>
+              <div class="flex mt-2 items-center">
+                <div
+                  class="spinner-border text-[#3263F3] animate-spin inline-block w-4 h-4 border-2 rounded-full"
+                  role="status"
+                >
+                  <span class="visually-hidden"></span>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <motion.div className="font-work-sans flex items-center mt-4 text-[#3263F3]">
+              <svg
+                width="14"
+                height="15"
+                viewBox="0 0 14 15"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M1 1.75L12.5 13.25"
+                  stroke="#EC5D6D"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                />
+                <path
+                  d="M1 13.25L12.5 1.75"
+                  stroke="#EC5D6D"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                />
+              </svg>
 
-            <motion.div className="ml-2 text-red-500">
-              Enter a Valid Email
+              <motion.div className="ml-2 text-red-500">
+                Enter a Valid Email
+              </motion.div>
             </motion.div>
-          </motion.div>
+          )
+        ) : loading ? (
+          <div>
+            
+          </div>
         ) : (
           <div className="font-work-sans flex items-center mt-4 text-[#3263F3]">
             <svg
